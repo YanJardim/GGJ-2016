e0b4e8740b0206e68cd1492072ba880d2a85d5c3
Gabriel Candido <gabriel.vccandido@gmail.com>
2016-01-30 20:49:24 -0200

    Pseudo-merge da server na master

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Net.Sockets;
+using System.Threading;
+
+public class Client : MonoBehaviour {
+    Thread thread = null;
+    NetworkStream stream;
+    System.IO.StreamReader sr;
+    System.IO.StreamWriter sw;
+    TcpClient client;
+
+    // Use this for initialization
+    void Start () {
+        // Get a client stream for reading and writing.
+        //  Stream stream = client.GetStream();
+        Connect("127.0.0.1");
+
+        stream = client.GetStream();
+        sw = new System.IO.StreamWriter(stream);
+        sr = new System.IO.StreamReader(stream);
+
+        thread = new Thread(LoopMensagem);
+        thread.Start();
+	}
+
+	void LoopMensagem () {
+        while (client.Connected)
+        {
+            if (stream.DataAvailable)
+            {
+                Debug.Log(sr.ReadLine());
+            }
+        }
+    }
+
+    void Send(string message)
+    {
+        sw.WriteLine(message);
+        sw.Flush();
+    }
+
+    void Connect(string server)
+    {
+        try
+        {
+            int port = 5050;
+            client = new TcpClient(server, port);
+
+            Debug.Log("Conectou!");
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        stream.Close();
+        client.Close();
+        thread.Abort();
+    }
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(100, 300, 500,500), "Mensagem"))
+        {
+            Send("Hello World!");
+        }
+    }
+}
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Net.Sockets;
+using System.Threading;
+
+public class Server : MonoBehaviour {
+    TcpListener server;
+    TcpClient client;
+    NetworkStream stream;
+    System.IO.StreamReader sr;
+    System.IO.StreamWriter sw;
+    Thread thread;
+
+    bool parar = false;
+    string mensagem = "Sem mensagem";
+
+    public int port;
+
+    // Use this for initialization
+    void Start () {
+        server = new TcpListener(System.Net.IPAddress.Loopback, port);
+        server.Start();
+
+        thread = new Thread(LoopMensagem);
+        thread.Start();
+    }
+
+
+    void Send(string message)
+    {
+        mensagem = "ENTROU AQUI";
+        sw.WriteLine(message);
+        sw.Flush();
+    }
+
+    void LoopMensagem()
+    {
+        while (!server.Pending())
+        {
+            mensagem = "Loop espera Pending";
+            Thread.Sleep(10);
+        }
+        client = server.AcceptTcpClient();
+        stream = client.GetStream();
+        sr = new System.IO.StreamReader(stream);
+        sw = new System.IO.StreamWriter(stream);
+        mensagem = "Conectado!!";
+
+        while (!parar)
+        {
+            mensagem = sr.ReadLine();
+            Send("FOIFOI");
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        // Close everything.
+        try
+        {
+            stream.Close();
+            client.Close();
+            parar = true; // Encerra o loop da thread, que se encerra em seguida
+        }
+        catch (System.Exception e)
+        {
+            mensagem = e.Message;
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(50, 50, 1000, 1000), mensagem);
+    }
+}
