18eadf6072fab8254ac7c83fc160befd6e89f7b5
Stephanie B <Stephanie B>
2016-01-30 15:44:01 -0200

    Pega e solta objectos!

---
+++ Assets/Scripts/PesonagemIteracao.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PesonagemIteracao : MonoBehaviour {
+	public GameObject item = null;
+	public int k;
+	void Update(){
+		k = this.GetComponentsInChildren<Transform> ().Length;
+		if (Input.GetKeyDown ("space")){
+			if (item != null && this.GetComponentsInChildren<Transform> ().Length == 2) {
+				item.transform.parent = this.transform;
+
+				//Send para desabilitar objeto
+			} else if (this.GetComponentsInChildren<Transform> ().Length > 2) {
+				item.transform.parent = null;
+
+				//Send para habilitar objeto
+			}
+		} 
+	}
+
+	void OnTriggerEnter(Collider other){
+		item = other.gameObject;
+
+		print ("Entrou");
+	}
+
+	void OnTriggerExit(Collider other){
+		item = null;
+
+		print ("Saiu");
+	}
+}
