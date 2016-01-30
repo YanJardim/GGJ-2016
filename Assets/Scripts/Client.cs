214fd7f3da3557f6c2a70961d79ed1d3e3546c57
andreluisribeiro <andreluis.cwb@gmail.com>
2016-01-30 06:02:03 -0200

    Hello World

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System;
+
+public class Client : MonoBehaviour {
+    bool connected = false;
+    TcpClient client;
+    NetworkStream stream;
+    System.IO.StreamWriter sw;
+
+    // Use this for initialization
+    void Start () {
+        Connect("127.0.0.1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(false && connected && stream.DataAvailable)
+        {
+            int i;
+            Byte[] bytes = new Byte[256];
+            string data; 
+
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                // Translate data bytes to a ASCII string.
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                Debug.Log("Received: "+ data);
+
+                // Process the data sent by the client.
+                data = data.ToUpper();
+                
+                Debug.Log("Sent: " + data);
+            }
+        }
+        
+    }
+
+    void Send(string message)
+    {
+        sw.WriteLine(message);
+        sw.Flush();
+    }
+
+    byte[] toByte(string str)
+    {
+        byte[] bytes = new byte[str.Length * sizeof(char)];
+        System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+        return bytes;
+    }
+
+    void Connect(string server)
+    {
+        try
+        {
+            int port = 5050;
+            client = new TcpClient(server, port);
+
+            // Get a client stream for reading and writing.
+            //  Stream stream = client.GetStream();
+
+            stream = client.GetStream();
+            sw = new System.IO.StreamWriter(stream);
+            Debug.Log("Conectou!");
+            connected = true;
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        stream.Close();
+        client.Close();
+    }
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(100, 300, 500,500), "Mensagem"))
+        {
+            Send("Hello World!");
+        }
+    }
+}
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Net.Sockets;
+
+public class Server : MonoBehaviour {
+    bool connected = false;
+    TcpClient client;
+    TcpListener server;
+    NetworkStream stream;
+    System.IO.StreamReader sr;
+
+    string mensagem = "Sem mensagem";
+
+    public int port;
+
+    byte[] toByte(string str)
+    {
+        byte[] bytes = new byte[str.Length * sizeof(char)];
+        System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+        return bytes;
+    }
+
+    // Use this for initialization
+    void Start () {
+        server = new TcpListener(System.Net.IPAddress.Loopback, port);
+        server.Start();
+
+        // Get a client stream for reading and writing.
+        //  Stream stream = client.GetStream();
+        
+    }
+
+    void Send(string message)
+    {
+        stream.Write(toByte(message), 0, message.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!connected)
+        {
+            if (!server.Pending()) return;
+            TcpClient client = server.AcceptTcpClient();
+            NetworkStream stream = client.GetStream();
+            sr = new System.IO.StreamReader(stream);
+            mensagem = "Conectado!!";
+            connected = true;
+        }
+
+        if (connected)
+        {
+            // if (!stream.DataAvailable) return;
+            
+            int i;
+            System.Byte[] bytes = new System.Byte[256];
+            string data;
+
+            mensagem = "Bytes";
+
+            mensagem = sr.ReadLine();
+
+            /*mensagem = "Bytes";
+
+            while (bytesCount != 0)
+            {
+                // Translate data bytes to a ASCII string.
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, bytesCount);
+               
+                // Process the data sent by the client.
+                mensagem = data;
+
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                
+                Debug.Log("Sent: " + data);
+                bytesCount = stream.Read(bytes, 0, bytes.Length);
+            }*/
+        }
+
+    }
+
+    void OnApplicationQuit()
+    {
+        // Close everything.
+        if(connected)
+        {
+            stream.Close();
+            client.Close();
+        }
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(50, 50, 1000, 1000), mensagem);
+    }
+}
