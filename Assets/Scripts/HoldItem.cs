e094e2f2c50f27e8f345266128c43dc7776f0bbf
andreluisribeiro <andreluis.cwb@gmail.com>
2016-01-31 13:03:15 -0200

    Adicionado PlaceHolder 2

--- Assets/Scripts/HoldItem.cs
+++ Assets/Scripts/HoldItem.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class HoldItem : MonoBehaviour {
     public GameObject item = null;
     public GameObject itemSegurado = null;
+    public Transform posicao;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -13,20 +15,30 @@ public class HoldItem : MonoBehaviour {
 	void Update () {
         if (Input.GetKeyDown("space"))
         {
-            if (item != null && this.GetComponentsInChildren<Transform>().Length == 3)
+            if (item != null && this.GetComponentsInChildren<Transform>().Length == 4)
             {
                 item.transform.parent = this.transform;
                 itemSegurado = item;
+                itemSegurado.transform.position = posicao.position;
+                itemSegurado.transform.GetChild(0).GetComponent<SpriteRenderer>().sortingOrder = 2;
+
+                
                 print("ASD");
                 //item.GetComponent<SpriteRenderer>().enabled = false;
 
                 GameObject.Find("Socket").SendMessage("SendObject", item);
             }
-            else if (this.GetComponentsInChildren<Transform>().Length > 3)
+            else if (this.GetComponentsInChildren<Transform>().Length > 4)
             {
                 itemSegurado.transform.parent = null;
-                itemSegurado = null;
+
+                Vector3 temp = itemSegurado.transform.position;
+                temp.z = 0;
+                itemSegurado.transform.position = temp;
+
+                itemSegurado.transform.GetChild(0).GetComponent<SpriteRenderer>().sortingOrder = 1;
                 GameObject.Find("Socket").SendMessage("SendObject", item);
+                itemSegurado = null;
             }
         }
 	}
